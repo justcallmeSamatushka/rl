629cb6817fd4a2aff2d1ef52d97aa2e677972460
Sam <justcallmeSamatushka>
2022-08-15 12:02:46 +0600

    EDIT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { DB_ENTITIES } from './utils/db-entities';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import path from "path";
-import { ConfigSet } from "ts-jest";
 import { UserEntity } from "./users/entity/users-entity";
 
 
@@ -19,6 +16,7 @@ import { UserEntity } from "./users/entity/users-entity";
         envFilePath: `.${process.env.NODE_ENV}.env`,
       })],
       useFactory: (config: ConfigService): TypeOrmModuleOptions => {
+        console.log("config", config.get("PGHOST"));
         return {
           type: 'postgres',
           host: config.get('PGHOST'),
